4dd61cdd22832f39b6d30607be9cd9f5ac86fd85
chidambarjoshi <chidambar.joshi5@gmail.com>
2021-04-14 17:40:50 +0530

    added ui and be code

--- covid19/covid19/settings.py
+++ covid19/covid19/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'covidapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'covid19.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ covid19/covidapp/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+import requests
+import json
+from dateutil.parser import parse
+
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "4ddfd41466msh069fbd82db6e9bbp1c4c1ejsn4dbe74d83eaf",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers).json()
+
+
+def  helloworldviews(request):
+    noofresults=int(response['results'])
+    mylist =[]
+    for x in range(0,noofresults):
+        mylist.append(response['response'][x]['country'])
+    print(type(mylist))
+    print(mylist.sort())
+    selectedcountry='All'
+    new=active=total=critical=recovered=deaths=newdeaths=""
+    if request.POST:
+        selectedcountry= request.POST['selectedcountry']
+        print(selectedcountry)
+
+    for x in range(0,noofresults):
+        if selectedcountry==response['response'][x]['country']:
+            new= response['response'][x]['cases']['new']
+            
+            active=response['response'][x]['cases']['active']
+            
+            total= response['response'][x]['cases']['total']
+            
+            critical=response['response'][x]['cases']['critical']
+            
+            recovered=response['response'][x]['cases']['recovered']
+            
+            deaths=response['response'][x]['deaths']['total']
+            
+            updatedtime=parse(response['response'][x]['time'])
+            
+            print(response['response'][x]['time'])
+    context={
+                'selectedcountry':selectedcountry,
+                'new':new,
+                'active':active,
+                'total':total, 
+                'critical':critical,
+                'recovered':recovered,
+                'deaths':deaths,
+                'updatedtime':updatedtime,
+                'mylist':sorted(mylist)
+            }
+    return render (request,'index.html',context)
